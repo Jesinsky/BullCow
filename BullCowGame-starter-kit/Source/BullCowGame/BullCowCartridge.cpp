d1a1e5e0c7b479dc92c8affc3f59904b0883c45a
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-18 12:53:00 -0500

    Setting Pseudo Code

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,16 +4,24 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Welcoming the player.
     PrintLine(TEXT("Elcome to Bull Cows!"));
     PrintLine(TEXT("Guess the 3 letter word!")); // Need to remove the number later on.
     PrintLine(TEXT("Press enter to continue..."));
+
+    // Setting up game.
     HiddenWord = TEXT("cow");
+    // Set lives.
+
+    // Prompt player for guess.
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cow");
+
+    // Checking players guess.
 
     if (Input == HiddenWord)
     {
@@ -23,5 +31,20 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         PrintLine(TEXT("You have lost!"));
     }
+    // Check if isogram.
+    // Prompt to guess again.
+    // Check right number of characters.
+    // Prompt to guess again.
+
+    // Remove life
+
+    // Check if lives > 0.
+    // If yes GuessAgain.
+    // Show lives left.
+    // If no show GameOver and HiddenWord.
+    // Prompt to play again, Press enter to PlayAgain.
+    // Check user input.
+    // PlayAgain or Quit.
+
 
 }
