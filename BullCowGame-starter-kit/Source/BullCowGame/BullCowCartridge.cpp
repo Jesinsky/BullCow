ce8c8976f06271dec998b44d1a5b5ed52f7227d5
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-26 15:06:06 -0500

    Creating an Isogram list
    Creating a valid isogram list based on HiddenWordList.h

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -9,13 +9,8 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     SetupGame();
 
     PrintLine(TEXT("The number of possible words is %i"), Words.Num());
+    PrintLine(TEXT("The number of valid words is: %i"), GetValidWords(Words).Num());
     PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord); // Debug line.
-
-    for (int32 Index = 0; Index < 5; Index++)
-    {
-        PrintLine(TEXT("%s"), *Words[Index]);
-    }
-    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -111,4 +106,18 @@ bool UBullCowCartridge::IsIsogram(FString Word) const
         
     }
     return true;
+}
+
+TArray<FString> UBullCowCartridge::GetValidWords(TArray<FString> WordList) const
+{
+    TArray<FString> ValidWords;
+
+    for (int32 Index = 0; Index < WordList.Num(); Index++)
+    {
+        if (WordList[Index].Len() >= 4 && WordList[Index].Len() <= 8 && IsIsogram(WordList[Index]))
+        {
+            ValidWords.Emplace(WordList[Index]);
+        }
+    }
+    return ValidWords;
 }
