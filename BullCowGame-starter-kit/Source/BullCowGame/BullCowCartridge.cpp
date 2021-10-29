f4b1253a86e9a1345923181a11c7f77ee07d2e8f
Jesus Moctezuma <40676930+Jesinsky@users.noreply.github.com>
2021-10-29 14:31:00 -0500

    Finishing the game
    Reorganize data to structs to keep clean the code and variables

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -88,10 +88,9 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
     }
 
     // Show the player Bulls and Cows.
-    int32 Bulls, Cows;
-    GetBullCows(Guess, Bulls, Cows);
+    FBullCowCount Score = GetBullCows(Guess);
 
-    PrintLine(TEXT("You have %i Bulls and %i Cows"), Bulls, Cows);
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
 
     PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
 }
@@ -129,25 +128,26 @@ TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList
     return ValidWords;
 }
 
-void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int32& CowCount) const
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
 {
-    BullCount = 0;
-    CowCount = 0;
+    FBullCowCount Count;
 
     for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
     {
         if (Guess[GuessIndex] == HiddenWord[GuessIndex])
         {
-            BullCount ++;
+            Count.Bulls++;
             continue;
         }
 
-        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); HiddenIndex ++)
+        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); HiddenIndex++)
         {
-            if (Guess[GuessIndex] == Guess[HiddenIndex])
+            if (Guess[GuessIndex] == HiddenWord[HiddenIndex])
             {
-                CowCount ++;
+                Count.Cows++;
+                break;
             }
         }
     }
+    return Count;
 }
